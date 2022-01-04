08add7490ebf5ffbc7022ceaa7776e07075a8ae9
chris stevens <cjs78@hotmail.co.uk>
2022-01-04 23:49:02 +0000

    first commit to github pages account

---
+++ precache-manifest.6ef934de2a6018308cf913965d3d5001.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24f56f8114bf0052137421719af0db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f819bfde28c6b037eff",
+    "url": "/static/css/2.cda56ea2.chunk.css"
+  },
+  {
+    "revision": "f9c5cef7326449ee88fd",
+    "url": "/static/css/main.b491e722.chunk.css"
+  },
+  {
+    "revision": "2f819bfde28c6b037eff",
+    "url": "/static/js/2.642ebfb0.chunk.js"
+  },
+  {
+    "revision": "f9c5cef7326449ee88fd",
+    "url": "/static/js/main.88672681.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "956a4255afb93debf3761bb19c92b0d6",
+    "url": "/static/media/chris-stevens.956a4255.png"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
